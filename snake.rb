d2139e0b019fd884052b8773e66612af2a29ef44
apet2929 <realandrewpetersen@gmail.com>
2023-06-05 13:09:34 -0400

    Rotating snake, body, apples

--- snake.rb
+++ snake.rb
@@ -3,6 +3,9 @@
 require 'gosu'
 
 SCALE = 1
+MARGIN = 50
+BG_SIZE = 32
+
 
 module Direction
   LEFT = [-1, 0]
@@ -11,50 +14,125 @@ module Direction
   DOWN = [0, 1]
 end
 
+def get_screen_pos(grid_x, grid_y)
+  return [(grid_x * BG_SIZE * SCALE) + MARGIN + grid_x,
+          (grid_y * BG_SIZE * SCALE) + MARGIN + grid_y
+  ]
+end
+
+def get_grid_pos(screen_x, screen_y)
+  x = (screen_x - MARGIN) / (BG_SIZE * SCALE)
+  y = (screen_y - MARGIN) / (BG_SIZE * SCALE)
+  return [x, y]
+end
+
 class Snake
   def initialize
-    @image = Gosu::Image.new("#{__dir__}/images/head.png")
-    @x = @y = 0.0
+    @head = Gosu::Image.new("#{__dir__}/images/head.png")
+    @body = Gosu::Image.new("#{__dir__}/images/body.png")
+    @x = @y = 0
     @history = []
-    @speed = 100
-    @length = 1
+    @speed = 2
+    @length = 3
     @direction = Direction::RIGHT
     p @direction
   end
 
   def key_press(left = false, right = false, up = false, down = false)
-    if left
+    gp = get_grid_pos(@x, @y)
+    p gp
+    gx = gp[0] - gp[0].floor
+    gy = gp[1] - gp[1].floor
+    p [gx, gy]
+
+
+    # if gy.abs > 0.5 && (left || right)
+    #   gp[1] += @direction[0]
+    # end
+    # if gx.abs > 0.5 && (up || down)
+    #   gp[0] += @direction[0]
+    # end
+
+    if left && @direction != Direction::RIGHT
       @direction = Direction::LEFT
-    elsif right
+      @y = @y.round # snap to nearest row/col
+    elsif right && @direction != Direction::LEFT
       @direction = Direction::RIGHT
-    elsif up
+      @y = @y.round # snap to nearest row/col
+    elsif up && @direction != Direction::DOWN
       @direction = Direction::UP
-    elsif down
+      @x = @x.round # snap to nearest row/col
+    elsif down && @direction != Direction::UP
       @direction = Direction::DOWN
+      @x = @x.round # snap to nearest row/col
     end
   end
 
-  def update(dt)
-    @history.pop
-    @history.append([@x, @y])
-
-    oldx = @x.round
-    oldy = @y.round
+  def update(dt, apple)
+    oldx = @x.floor
+    oldy = @y.floor
 
     @x += @direction[0] * @speed * dt
     @y += @direction[1] * @speed * dt
-    newx = @x.round
-    newy = @y.round
-
+    newx = @x.floor
+    newy = @y.floor
 
     if oldx != newx || oldy != newy
-    #   entered new square
+      if @history.length == @length
+        @history.delete_at(0)
+      end
+      @history.append([oldx, oldy])
+      p [@x, @y]
     end
 
+
+
+    if intersects(apple[0], apple[1])
+      @length += 1
+      return true
+    end
+    return false
   end
 
   def draw()
-    @image.draw(@x, @y, 0, SCALE, SCALE)
+    pos = get_screen_pos(@x, @y)
+    pos[0] += BG_SIZE / 2
+    pos[1] += BG_SIZE / 2
+
+    if @direction == Direction::LEFT
+      @head.draw_rot(pos[0], pos[1], 0, 180, 0.5, 0.5, SCALE, SCALE)
+    elsif @direction == Direction::RIGHT
+      @head.draw_rot(pos[0], pos[1], 0, 0, 0.5, 0.5, SCALE, SCALE)
+    elsif @direction == Direction::UP
+      @head.draw_rot(pos[0], pos[1], 0, 270, 0.5, 0.5, SCALE, SCALE)
+    elsif @direction == Direction::DOWN
+      @head.draw_rot(pos[0], pos[1], 0, 90, 0.5, 0.5, SCALE, SCALE)
+    end
+
+    for pos in @history
+      bp = get_screen_pos(pos[0], pos[1])
+      bp[0] += BG_SIZE / 2
+      bp[1] += BG_SIZE / 2
+      @body.draw_rot(bp[0], bp[1], 0, 0, 0.5, 0.5, SCALE, SCALE)
+    end
+  end
+
+  def intersects(grid_x, grid_y)
+
+    # p "Snake(#{[@x, @y]}), Apple(#{[grid_x, grid_y]})"
+    if @x.floor == grid_x && @y.floor == grid_y
+      return true
+    end
+
+    for pos in @history
+      gx, gy = get_grid_pos(pos[0], pos[1])
+      gx = gx.floor
+      gy = gy.floor
+      if gx == grid_x && gy == grid_y
+        return true
+      end
+    end
+    return false
   end
 end
 
@@ -66,12 +144,12 @@ class Window < Gosu::Window
 
     @keyboard = Gosu::Image.new("#{__dir__}/images/keyboard.png")
     @background = Gosu::Image.new("#{__dir__}/images/grass.png")
+    @apple_tex = Gosu::Image.new("#{__dir__}/images/apple.png")
     @width = 20
     @height = 20
-    @bg_size = 32
-    @margin = 50
     @snake = Snake.new
     @time = -1.0
+    @apple = []
 
   end
 
@@ -79,26 +157,37 @@ class Window < Gosu::Window
     for i in 0..@width
       for j in 0..@height
         # I add i and j here to add a 1 pixel gap between each tile
-        x = (i * @bg_size * SCALE) + @margin + i
-        y = (j * @bg_size * SCALE) + @margin + j
+        x, y = get_screen_pos(i, j)
         @background.draw(x, y, 0, SCALE, SCALE)
       end
     end
-    @snake.draw()
+    if @apple != nil
+      apos = get_screen_pos(@apple[0], @apple[1])
+      @apple_tex.draw(apos[0], apos[1], 0, SCALE, SCALE)
+      gp = get_grid_pos(apos[0], apos[1])
+      sp = get_screen_pos(gp[0], gp[1])
+      @apple_tex.draw(sp[0], sp[1], 0, SCALE, SCALE)
+    end
 
+    @snake.draw()
 
   end
 
   def update
     if @time == -1
       @time = Gosu.milliseconds
+      @apple = viable_apple_pos
       return
     end
+
     dt = (Gosu.milliseconds - @time) / 1000.0
     @time = Gosu.milliseconds
 
-    @snake.update(dt)
-
+    spawn_apple = @snake.update(dt, @apple)
+    if spawn_apple
+      @apple = viable_apple_pos
+      p "Apple(#{@apple}"
+    end
   end
 
   def button_down(id)
@@ -113,6 +202,14 @@ class Window < Gosu::Window
     else
       puts "Hello world!"
     end
+  end
+
+  def viable_apple_pos
+    pos = [(Random.rand() * @width).floor, (Random.rand() * @height).floor]
+    while @snake.intersects(pos[0], pos[1])
+      pos = [(Random.rand() * @width).floor, (Random.rand() * @height).floor]
+    end
+    pos
   end
 end
 
